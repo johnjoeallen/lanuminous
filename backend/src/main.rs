04179a45cb3b714fec2d6811c03bba4aee52eac3
John Allen <john.allen@dublinux.net>
2026-03-17 09:08:46 +0000

    Update backend main crate imports for rename

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,13 +1,13 @@
 use anyhow::Result;
 use clap::Parser;
-use lantricate::cli::Cli;
+use lanuminous::cli::Cli;
 use tracing_subscriber::EnvFilter;
 
 #[tokio::main]
 async fn main() -> Result<()> {
     tracing_subscriber::fmt()
         .with_env_filter(
-            EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("lantricate=info")),
+            EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("lanuminous=info")),
         )
         .with_target(false)
         .compact()
